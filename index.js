d4966c18b597d49b27f5b865699f21ef304ef28d
Dennis Galasao <dennisgalasao2.0@gmail.com>
2026-05-11 14:24:36 +0800

    Fixed event handling

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const paragraph = document.getElementById("parag1");
 const button1 = document.querySelector("#btn1");
 const button2 = document.querySelector("#btn2");
 
-// Click event for Alert Me button
 button1.addEventListener("click", () => {
     alert("Welcome to IT10");
 });
 
-// Double click event for Change Color button
 button2.addEventListener("dblclick", () => {
     paragraph.style.color = "red";
 });
